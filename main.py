451b23b2a5d1643550110ccd24ef03d5af13878a
Maxutka1 <91219728+Maxutka1@users.noreply.github.com>
2024-11-30 00:48:19 +0300

    Added development

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def add(a, b):
     return a + b
 
-if __name__ == "__main__":
-    print("2 + 3 =", add(2, 3))
+def subtract(a, b):
+    return a - b
+
+if name == "__main__":    print("2 + 3 =", add(2, 3))
+    print("5 - 3 =", subtract(5, 3))
