5c031d4701854aa2991f088350c58fb21f172605
Maxutka1 <91219728+Maxutka1@users.noreply.github.com>
2024-11-30 01:16:56 +0300

    Added new function development

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def add(a, b):
     return a + b
 
 def new_function(): 
-    return "New feature added!"
+    return "New feature added!
 
 if __name__ == "__main__":    
     print("2 + 3 =", add(2, 3))
