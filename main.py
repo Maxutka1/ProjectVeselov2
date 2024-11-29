21dc5f243214292f9459afa3eba702f04d1dec8d
Maxutka1 <91219728+Maxutka1@users.noreply.github.com>
2024-11-30 01:28:41 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def add(a, b):
     return a + b
 
-def new_function(): 
-    return "New feature added!"
+def subtract(a, b):
+    return a - b
 
-if __name__ == "__main__":    
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
     print("2 + 3 =", add(2, 3))
+    print("5 - 3 =", subtract(5, 3))
+    print("4 * 2 =", multiply(4, 2))
